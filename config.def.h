ef2f7297853cbf08959ebbd5eb22ec14eede1806
Wako Diwe <wakodiwe@gmail.com>
2023-12-25 09:38:41 +0100

    Add additional keys for scrolling

--- config.def.h
+++ config.def.h
@@ -209,6 +209,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ MODKEY,               XK_k,           kscrollup,      {.i = -1} },
+	{ MODKEY,               XK_j,           kscrolldown,    {.i = -1} },
 };
 
 /*
