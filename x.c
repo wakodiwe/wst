2082316a65f25c010d28f571ddbb24a567ea1270
Wako Diwe <wakodiwe@gmail.com>
2023-12-25 09:59:18 +0100

    Add options for xresources disable bold italic roman

--- x.c
+++ x.c
@@ -2105,7 +2105,9 @@ xrdb_load(void)
 		XRESOURCE_LOAD_INTEGER("bellvolume", bellvolume);
 		XRESOURCE_LOAD_INTEGER("borderpx", borderpx);
 		XRESOURCE_LOAD_INTEGER("cursorshape", cursorshape);
-
+		XRESOURCE_LOAD_INTEGER("disablebold", disablebold);
+		XRESOURCE_LOAD_INTEGER("disableitalic", disableitalic);
+		XRESOURCE_LOAD_INTEGER("disableroman", disableroman);
 		XRESOURCE_LOAD_FLOAT("cwscale", cwscale);
 		XRESOURCE_LOAD_FLOAT("chscale", chscale);
 	}
